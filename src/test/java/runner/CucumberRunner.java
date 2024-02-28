77ef6555b04da1a3e314e93a23ea973f6480897f
menakanirmalkumar <133275215+menakanirmalkumar@users.noreply.github.com>
2024-02-28 17:55:23 -0500

    UpdatedRunner
    UpdatedRunner

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
 
-@CucumberOptions(tags = "@RegTest", features = "src/test/resources/features", glue = "stepDefinitions", plugin = {
+@CucumberOptions(tags = "", features = "src/test/resources/features", glue = "stepDefinitions", plugin = {
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/CucumberTestReport.json",
 		"html:target/CucumberTestReport.html",})
 public class CucumberRunner  extends AbstractTestNGCucumberTests {
