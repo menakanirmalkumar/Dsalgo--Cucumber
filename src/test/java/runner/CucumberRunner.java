595b7f4b0d91cfaa11531eac4d72e0757b3c42a1
menakanirmalkumar <133275215+menakanirmalkumar@users.noreply.github.com>
2024-02-29 00:37:31 -0500

    reportallureupdate
    reportallureupdate

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -9,26 +9,24 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
-
 @CucumberOptions(tags = "", features = "src/test/resources/features", glue = "stepDefinitions", plugin = {
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/cucumber-reports/cucumber.json",
-		"html:target/CucumberTestReport.html","junit:target/report.html"})
-public class CucumberRunner  extends AbstractTestNGCucumberTests {
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "pretty",
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "json:target/cucumber-reports/cucumber.json",
+		"html:target/CucumberTestReport.html", "junit:target/report.html" })
+public class CucumberRunner extends AbstractTestNGCucumberTests {
 
-   @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
-   
-  
-   @BeforeClass
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
+
+	@BeforeClass
 	@Parameters({ "browser" })
 
 	public void defineBrowser(String browser) {
 
 		ConfigReader.setBrowserType(browser);
-
 
 	}
 
